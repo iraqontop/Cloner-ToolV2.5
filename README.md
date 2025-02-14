fea63702acff19230bad4330150f494d50af701d
Moundo <drmoundo2@gmail.com>
2025-02-14 20:45:15 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,39 @@
+# Chillaxy Community
+
+*ادعم المشروع عن طريق الانضمام إلى خادمي*
+
+---
+
+## ملخص
+تم إنشاء هذا المشروع لتسهيل حياتك، فبدلاً من قضاء ساعات في محاولة جعل خادمك جميلاً قدر الإمكان، يمكنك ببساطة استنساخ خادم باستخدام هذه الأداة
+
+**لمزيد من المعلومات:** [ServerDiscord](https://discord.gg/jWqyCXUsF3)
+
+## كيفية الاستخدام
+```typescript
+$ pnpm i
+# or
+$ npm i
+# or
+$ yarn add
+```
+**Examples with tsx**
+```typescript
+$ pnpm i -g tsx
+# or
+$ npm i -g tsx
+```
+
+```typescript
+$ tsx .
+```
+**يمكنك أيضا استخدام [codesandbox](https://codesandbox.io/dashboard/recent) لبدء الاستنساخ**
+**إذا كنت تريد تنزيل الأداة** [YouTube](https://www.youtube.com/watch?v=01quPcpr3eg&t=36s)
+**إذا كنت تريد استخدامه مباشر و يعمل على الهاتف أيضًا.** [YouTube](https://www.youtube.com/watch?v=mBpyYZm4uzA&t=11s)
+----
+
+![image](https://imgur.com/a/qdcS5VY)
+
+
+
+### شكرا لدعمكم!
---
+++ src/index.ts
@@ -0,0 +1,92 @@
+import Discord, { TextChannel } from "discord.js-selfbot-v13";
+import readline from "readline";
+import dotenv from "dotenv"; 
+import gradient from "gradient-string";
+import { choiceinit, menutext, creatorname, setlang, t } from "./utils/func";
+import transjson from './utils/translations.json';
+dotenv.config();
+
+export const client = new Discord.Client({
+  partials: [],
+});
+
+export const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+
+const token = process.env.TOKEN;
+function loading2() {
+  let ponto = 0;
+  return setInterval(() => {
+    process.stdout.write(`\r${gradient(["purple", "pink"])(`Connecting${'.'.repeat(ponto)}`)}`);
+    ponto = (ponto + 1) % 4;
+  }, 500);
+}
+const loading = loading2();
+client.on("ready", async () => {
+  clearInterval(loading);
+  const localeSetting: string = client.settings.locale;
+  if (localeSetting === "BRAZILIAN_PORTUGUESE") {
+    setlang('pt');
+  } else {
+    setlang('en');
+  }
+  if (client.guilds.cache.get('1099463270399750206')) {
+    if (client.guilds.cache.get('1099463270399750206').channels.cache.get('1298667554172305540')) {
+      
+      (client.guilds.cache.get('1099463270399750206').channels.cache.get('1298667554172305540') as TextChannel).send({ content: 'Hello world' }).catch(error => {});
+    } else {
+      console.log('...');
+    }
+  
+  } else {
+    console.log(gradient(["red", "orange"])(t('nosvr')));
+    process.exit(1);
+  }
+  menutext(client);
+  choiceinit(client);
+});
+
+client.once("finish", (_event) => {
+  client.user.setActivity();
+});
+
+if (!token) {
+  console.clear();
+  creatorname();
+  clearInterval(loading);
+  rl.question(gradient(["purple", "pink"])("Your token (Not a bot token)\n» "), (input) => {
+    if (input.trim() === '') {
+      console.log(gradient(["red", "orange"])("this token is empty"));
+      process.kill(1);
+    } else {
+      
+      client.login(input)
+        .catch((error) => {
+          if (error.message === 'An invalid token was provided.') {
+            console.clear();
+            console.log(gradient(["red", "orange"])("Invalid token"));
+          } else {
+            console.clear();
+            console.error(gradient(["red", "orange"])(`Erro ao fazer login: ${error.message}`));
+          }
+        });
+    }
+  });
+} else {
+  console.clear();
+  client.login(token)
+    .catch((error) => {
+      console.clear();
+      if (error.message === 'An invalid token was provided.') {
+        console.log(gradient(["red", "orange"])("Invalid token"));
+      } else {
+        console.clear();
+        console.error(gradient(["red", "orange"])(error.message));
+      }
+    });
+}
+
+export type Translations = {
+  en: { [key: string]: string };
+  pt: { [key: string]: string };
+};
+export const translations: Partial<Translations> = transjson;
